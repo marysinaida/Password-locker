3557eb5efbddbb94aea06cdc77d7c9d432a0893f
marysinaida <marydorcassinaida54@gmail.com>
2019-11-08 17:30:47 +0300

    add a clean up method

--- test.py
+++ test.py
@@ -28,6 +28,13 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user() #saving the new user
         self.assertEqual(len(User.user_list),1)
+
+    def tearDown(self):
+
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
         
 
 

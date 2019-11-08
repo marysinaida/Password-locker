e947b8e64db94f84f48958b5c35d98f83ad07236
marysinaida <marydorcassinaida54@gmail.com>
2019-11-08 17:16:50 +0300

    add a method

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 from main import User
-from main import Credentials
+# from main import Credentials
 
 class TestUser(unittest.TestCase):
     '''
@@ -10,14 +10,27 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         '''Set up method to run before each test cases.
         '''
-        self.new_user = User("Snyder","345@Snyder")#create user object
+        self.new_user = User("Dorcas","Mary","0765432112","345@Snyder","Snyder@sm.com")#create user object
 
     def test_init(self):
         '''
         Test if the object is properly initialized
         '''
-        self.assertEqual(self.new_user.user_name,"Snyder")
-        self.assertEqual(self.new_user.user_password,"345@snyder") 
+        self.assertEqual(self.new_user.first_name,"Dorcas")
+        self.assertEqual(self.new_user.last_name,"Mary")
+        self.assertEqual(self.new_user.phone_number,"0765432112")
+        self.assertEqual(self.new_user.password,"345@Snyder")
+        self.assertEqual(self.new_user.email,"Snyder@sm.com") 
+
+    def test_save_user(self):
+        '''
+        Test_save_user test case to test if the user object is saved into the user list
+        '''
+        self.new_user.save_user() #saving the new user
+        self.assertEqual(len(User.user_list),1)
+        
+
+
 
 if __name__=='__main__':
     unittest.main()   

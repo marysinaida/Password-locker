ec0ff9a0da6c8d186246cca33cc862a7b46c22e9
marysinaida <marydorcassinaida54@gmail.com>
2019-11-08 17:15:57 +0300

    add a method

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ class User:
 
         self.first_name = first_name
         self.last_name = last_name
-        self.phone_number = number
+        self.phone_number = phone_number
         self.password = password
         self.email = email
+
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)

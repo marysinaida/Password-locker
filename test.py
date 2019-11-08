e22c2704a65da1bcda4773a25991add169d89ac9
marysinaida <marydorcassinaida54@gmail.com>
2019-11-08 17:50:35 +0300

    add a delete method

--- test.py
+++ test.py
@@ -35,7 +35,17 @@ class TestUser(unittest.TestCase):
         tearDown method that does clean up after each test case has run.
         '''
         User.user_list = []
-        
+
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a contact from our contact_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Dorcas","Mary","0765432112","345@Snyder","Snyder@sm.com")#new user
+        test_user.save_user()
+
+        self.new_user.delete_user()#Deleting a user object
+        self.assertEqual(len(User.user_list),1)
 
 
 

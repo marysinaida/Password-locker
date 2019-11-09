7c8f21568cd44c2aba77ca40ec1e62ae7af1f4ff
marysinaida <marydorcassinaida54@gmail.com>
2019-11-09 12:00:49 +0300

    add a method that confirms password and user name

--- main.py
+++ main.py
@@ -37,6 +37,19 @@ class Credentials:
     credentials_list = [] #empty credential list
     user_credentials_list = []
 
+    @classMethod
+    def check_user(cls,username,password):
+        '''
+        method that confirms the user_list match with the keyed password and name.
+        '''
+        current_user = ''
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                current_user = user.username
+                return current_user
+
+     
+
 
     def __init__(self,user_name,account_name,password):
         

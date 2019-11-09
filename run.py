eb26742ce26f4fa94bf53b9ffe514e68436b0de2
marysinaida <marydorcassinaida54@gmail.com>
2019-11-09 19:51:25 +0300

    add mrthods and functions

--- run.py
+++ run.py
@@ -15,12 +15,6 @@ def create_credntials(user_name,account_name,password):
     new_credential =Credentials(user_name,account_name,password):
     return new_credential
 
-def save_use(user):
-    '''
-    function to save a new user account
-    '''
-    User.save_user(user)
-
 def save_user(user):
     '''
     function to save a new user account
@@ -39,3 +33,33 @@ def verify_user(user,password):
     '''
     checking_user = Credentials.chek_user(username,password)
     return checking_user
+
+def rand_pass(size):
+    '''
+    a function that combines random letters and digits to generate new password
+    '''
+    return Credential.rand_pass(size)
+
+def display_credentials(user_name):
+    '''
+    Aclass method that display all credentials of a given username.
+    '''
+    return Credentials.display_credentials(user_name)
+
+def delete_credentials(credentials):
+    '''
+    Function to delete Credentials from credentials list
+    '''
+    credentials.delete_credentials()
+
+def find_by_account_name(site_name):
+    '''
+    A function to search for an account using the credentials given
+    '''
+    return Credentials.find_by_account_name(account_name)
+
+def copy_credentials(account_name):
+    '''
+    A method to enable copying credentials of a given account name.
+    '''
+    return Credentials.copy_credentials(account_name)

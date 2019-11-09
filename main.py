bce5a4013ea2e94fc75578abf70c4c409310a1b8
marysinaida <marydorcassinaida54@gmail.com>
2019-11-09 14:47:40 +0300

    import the xclip

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import string
+import random
+import pyperclip
+
 class User:
     '''
     Class that generate new instances of users
@@ -72,6 +76,19 @@ class Credentials:
         '''
         Credentials.credentials_list.remove(self)
 
+
+    @classmethod
+    def rand_pass(cls,size):
+        '''
+        method to generate password that takes random choice from and ascii_letters and digits
+        '''
+
+        generate_pass =''.join([random.choice(string.ascii_letters + string.digits) for n in range(size)])
+
+        return generate_pass
+
+     
+
     @classmethod
     def find_by_account_name(cls,account_name):
         '''
@@ -79,5 +96,26 @@ class Credentials:
         '''
         for credential in cls.credentials_list:
             if credential.account_name == account_name:
-                return credential  
- 
+                return credential 
+
+    @classmethod
+    def display_credentials(cls,user_name):
+        '''
+        method to display credentials list saved
+        '''
+        user_credentials_list = []
+        for credential in cls.credentials_list:
+            if credential.user_name == user_name:
+                user_credentials_list.append(credential)
+        return user_credentials_list
+
+    @classmethod
+    def copy_credentials(cls,account_name):
+        '''
+        A method that copies credentials information after account is entered.
+        '''
+        found_credential = cls.find_by_account_name(account_name)
+        return pyperclip.copy(found_credential.password)
+
+
+    
